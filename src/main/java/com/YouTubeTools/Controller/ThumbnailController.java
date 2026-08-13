c28a6606f97219b95b6d203ba1818c0e58f5dbaf
Aman Kumar <aman@Amans-MacBook-Air.local>
2026-08-14 04:25:09 +0530

    Added page and thumbnail conrollers

---
+++ src/main/java/com/YouTubeTools/Controller/ThumbnailController.java
@@ -0,0 +1,13 @@
+package com.YouTubeTools.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThumbnailController {
+
+    @GetMapping("/thumbnail")
+    public String getThumbnails() {
+        return "thumbnails";
+    }
+}
